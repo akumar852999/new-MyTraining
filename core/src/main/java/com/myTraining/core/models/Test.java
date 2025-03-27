61dadfb32546344af4852d920c602553b04d68cd
Amit Kumar <r.kumar@ubiquedigital.com>
2025-03-27 09:24:19 +0530

    issue fixed

--- core/src/main/java/com/myTraining/core/models/Test.java
+++ core/src/main/java/com/myTraining/core/models/Test.java
@@ -1,11 +1,15 @@
 package com.myTraining.core.models;
 
+import com.myTraining.core.services.CustomConfig;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 
 import javax.annotation.Resource;
 
-@Model(adaptables = Resource.classrce.class)
+@Model(adaptables = Resource.class)
 public class Test {
 
+    @OSGiService
+    CustomConfig ci;
 
 }

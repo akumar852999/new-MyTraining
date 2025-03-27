797c9695ac955e6db3a80a0beb14134a0bc78f31
Amit Kumar <r.kumar@ubiquedigital.com>
2025-03-27 09:22:04 +0530

    created test model

---
+++ core/src/main/java/com/myTraining/core/models/Test.java
@@ -0,0 +1,11 @@
+package com.myTraining.core.models;
+
+import org.apache.sling.models.annotations.Model;
+
+import javax.annotation.Resource;
+
+@Model(adaptables = Resource.classrce.class)
+public class Test {
+
+
+}
